23253426f7f9a9355b85fe921d11707864f89553
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-31 01:02:50 +0300

    add state on the expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem({ date, title, amount }) {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const clickHandler = () => {
+    setExpenseTitle('Updated');
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>${amount.toFixed(2)}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
