24c55c092302955456a0ce3fc067432ad246304d
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-30 22:39:48 +0300

    split date of expense item to its own component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem({ date, title, amount }) {
-  const month = date.toLocaleDateString('en-US', { month: 'long' });
-  const day = date.toLocaleDateString('en-US', { day: '2-digit' });
-  const year = date.toLocaleDateString('en-US', { year: 'numeric' });
-  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div> {day}</div>
-        <div> {year}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount.toFixed(2)}</div>
