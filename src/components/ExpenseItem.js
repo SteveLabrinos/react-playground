43bfe5fb6a99788b54e1fc6a74edc32f6e92e877
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-30 22:16:38 +0300

    create list of expense items with dynamic data assignment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem({ date, title, amount }) {
+  const month = date.toLocaleDateString('en-US', { month: 'long' });
+  const day = date.toLocaleDateString('en-US', { day: '2-digit' });
+  const year = date.toLocaleDateString('en-US', { year: 'numeric' });
+  
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div> {day}</div>
+        <div> {year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$295.67</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount.toFixed(2)}</div>
       </div>
     </div>
   );
