6eb0c8e73629df8668d47b50f69c21c17607f1d4
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-31 13:15:38 +0200

    add expense form component to let user create new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -30,6 +31,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
