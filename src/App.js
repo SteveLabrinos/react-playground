9ddc056faa6affbb9fd07a767c893ba8848773d9
SteveLabrinos <slabrinos9214@gmail.com>
2021-11-01 23:21:06 +0200

    change item generated id that was producing a bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
@@ -6,46 +6,54 @@ import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    {text: 'Do all exercises!', id: 'g1'},
-    {text: 'Finish the course!', id: 'g2'},
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' },
   ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({text: enteredText, id: 'goal1'});
+      updatedGoals.unshift({
+        text: enteredText,
+        id: 'goal1',
+        // id: Math.random().toString(),
+      });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter(
+        (goal) => goal.id !== goalId
+      );
       return updatedGoals;
     });
   };
 
   let content = (
-    <p style={{textAlign: 'center'}}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: 'center' }}>
+      No goals found. Maybe add one?
+    </p>
   );
 
   if (courseGoals.length > 0) {
     content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler}/>
+      <CourseGoalList
+        items={courseGoals}
+        onDeleteItem={deleteItemHandler}
+      />
     );
   }
 
   return (
-
-    <section id="goal-form">
-      <CourseInput onAddGoal={addGoalHandler}/>
-    </section>
-  <section id="goals">
-    {content}
-  </section>;
-
-)
-  ;
+    <React.Fragment>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
+    </React.Fragment>
+  );
 };
 
 export default App;
