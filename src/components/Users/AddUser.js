24a1d7f2d79b9f441cf701b9ebbf37ba327e6671
SteveLabrinos <slabrinos9214@gmail.com>
2021-11-02 22:36:47 +0200

    change input fields to uncotrolled state with refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,18 +6,11 @@ import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css';
 
 function AddUser({ onAddUser }) {
-  const [enteredData, setEnteredData] = useState({
-    username: '',
-    age: '',
-  });
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [showErrorModal, setShowErrorModal] = useState(false);
   const [modalContent, setModalContent] = useState();
-
-  const changeInputHandler = property => event => {
-    setEnteredData(prevData => {
-      return { ...prevData, [property]: event.target.value };
-    });
-  };
 
   const showErrorHandler = content => {
     setShowErrorModal(true);
@@ -30,23 +23,22 @@ function AddUser({ onAddUser }) {
 
   const submitHandler = event => {
     event.preventDefault();
-    if (enteredData.username.trim().length === 0 || enteredData.age.trim().length === 0) {
+    const username = nameInputRef.current.value;
+    const age = ageInputRef.current.value;
+    if (username.trim().length === 0 || age.trim().length === 0) {
       showErrorHandler({
         title: 'An error has occurred',
         message: 'Username and Age fields must have a value',
       });
-    } else if (+enteredData.age <= 0) {
+    } else if (+age <= 0) {
       showErrorHandler({
         title: 'An arithmetic error has occurred',
         message: 'Age must be a positive integer',
       });
     } else {
-      onAddUser(enteredData);
-      // Reset the form inputs
-      setEnteredData({
-        username: '',
-        age: '',
-      });
+      onAddUser({ username, age });
+      nameInputRef.current.value = '';
+      ageInputRef.current.value = '';
     }
   };
 
@@ -56,19 +48,9 @@ function AddUser({ onAddUser }) {
       <Card className={styles.input}>
         <form onSubmit={submitHandler}>
           <label htmlFor='username'>Username</label>
-          <input
-            onChange={changeInputHandler('username')}
-            value={enteredData.username}
-            id='username'
-            type='text'
-          />
+          <input ref={nameInputRef} id='username' type='text' />
           <label htmlFor='age'>Age (Years)</label>
-          <input
-            onChange={changeInputHandler('age')}
-            value={enteredData.age}
-            id='age'
-            type='number'
-          />
+          <input ref={ageInputRef} id='age' type='number' />
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
