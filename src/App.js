996c92d38a2ef47d2f1af51355633ddefad7d88d
SteveLabrinos <slabrinos9214@gmail.com>
2021-11-02 23:16:32 +0200

    add useEffect hook to check user token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -10,10 +10,20 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
+  useEffect(() => {
+    const userInfo = localStorage.getItem('isLoggedIn');
+
+    if (userInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
