e8e68deb631029c6cc90503b609983208ceb8679
SteveLabrinos <slabrinos9214@gmail.com>
2021-11-04 16:07:38 +0200

    add logout handler with context API

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}>
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
