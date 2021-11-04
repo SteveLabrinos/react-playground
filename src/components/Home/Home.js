82dc98b869b4c6ee8d549e1566346c9bb294e016
SteveLabrinos <slabrinos9214@gmail.com>
2021-11-04 15:24:46 +0200

    add central state management with context API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Card from '../UI/Card/Card';
+import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = ({ onLogout }) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Logout</Button>
     </Card>
   );
 };
